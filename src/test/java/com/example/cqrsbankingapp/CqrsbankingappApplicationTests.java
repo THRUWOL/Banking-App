6721fd3100f4ced4939b916aed83164fb1ab9f8b
NikitaYarosh <yarosh.nv@yandex.ru>
2024-05-01 11:02:41 +0300

    feature: add spring app template

---
+++ src/test/java/com/example/cqrsbankingapp/CqrsbankingappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cqrsbankingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CqrsbankingappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
